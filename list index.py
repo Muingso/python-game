9d2ed7a762d351f4c6e8e8725b795337d6ba5789
Muingso <sureshwanem22@gmail.com>
2025-02-19 19:07:38 +0000

    Add files via upload

---
+++ list index.py
@@ -0,0 +1,31 @@
+#1D list
+
+L1 = [1,22,"Hi",2.5,False]
+L1.append("3")
+L1.insert(2,"M")
+
+print(L1[3:6])
+
+print(L1)
+
+fruits = ["apple","banana","cherry"]
+
+print(fruits[0])
+print(fruits[1])
+
+fruits.append("orange")
+print(fruits)
+
+fruits.remove("banana")
+print(fruits)
+print(fruits[1:3])
+#2-D list
+L2=[["dog","mouse","duck"],
+    ["lion","tiger","leoperd"],
+    ["elephant","rihon","girffare"]]
+A1=L2[0][1]
+print(A1)
+print(L2[1][2])
+
+L2.append(["cat","something"])
+print(L2)
