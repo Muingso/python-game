fe6b821d0058d5208ff377aa2cb3d7436bfa07e5
Muingso <sureshwanem22@gmail.com>
2025-03-05 18:57:09 +0000

    Add files via upload

---
+++ Tuples intro.py
@@ -0,0 +1,12 @@
+address = ('250','Geribal street','Shbuya','France','12345')#this is packing
+for i in address:
+    print(i)
+# printing all the vals inside the tuple
+h_no,street,city,country,postalcode=address##this is unpacking
+print(h_no)
+print(street)
+print(city)
+print(country)
+print(postalcode)
+t1=3,4,"dog",2.5
+print(t1)
