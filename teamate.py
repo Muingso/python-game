d3bd64639add2d67dea39f43780b04cbec0ce43a
Muingso <sureshwanem22@gmail.com>
2025-03-12 18:42:55 +0000

    Add files via upload

---
+++ teamate.py
@@ -0,0 +1,24 @@
+chessset =('eight pawns','two bishops','two knights','two rooks','one queen','one king')
+for i in chessset:
+    print (i)
+
+print (chessset [1:3])
+print (chessset [3:6])
+
+t1 = (1,2,3)
+t2 = (4,5,6)
+# TUples concatenation
+t3 = t1 + t2
+
+print (t3)
+
+t4 = t1 * 2
+print (t4)
+
+t5 = t3 * 3
+print (t5)
+
+t6 = t5 * 999
+print (t6)
+ 
+print(len(t4))
